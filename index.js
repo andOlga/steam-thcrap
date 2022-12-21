558098faa33bbca9a557df3498b8592796df3994
ooa113y <462484+ooa113y@users.noreply.github.com>
2022-12-22 00:04:10 +0400

    Generate sensible game names

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fd = require('node-file-dialog')
 const ss = require('steam-shortcut-editor')
 const fs = require('fs')
+const names = require('./names.json')
 
 function makeSteamPath (path) { // Converts a normal path into the Steam format
   return `"${path.replaceAll('/', '\\')}"`
@@ -36,7 +37,15 @@ async function main () {
       for (const lang of thLangs) {
         for (const game of Object.keys(thGames)) {
           if (game.endsWith('_custom')) continue // Skip "custom" entries
-          const appName = `${game} (${lang.replace('.js', '')})`
+          // Generate app name for Steam
+          let appName = game
+          if (appName.length > 4) appName = appName.slice(0, 4) + '.' + appName.slice(4) // "th095" -> "th09.5"
+          appName = appName.replace('th', 'Touhou ') // "th09.5" -> "Touhou 09.5"
+          if (names[game]) {
+            appName += `: ${names[game]}` // "Touhou 09.5" -> "Touhou 09.5: Shoot the Bullet"
+          }
+          appName += ` (${lang.replace('.js', '')})` // "Touhou 09.5: Shoot the Bullet" -> "Touhou 09.5: Shoot the Bullet (en)"
+          // Actually add the game
           console.log(`Adding ${appName}...`)
           const short = {
             AppName: appName,
