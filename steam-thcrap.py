a82a7b9955d90b7c47b766c2ac1bfe2248f88201
ooa113y <462484+ooa113y@users.noreply.github.com>
2022-12-27 22:36:14 +0400

    Some reformatting

--- steam-thcrap.py
+++ steam-thcrap.py
@@ -24,7 +24,7 @@ names = {
     "th17": "Wily Beast and Wicked Creature",
     "th175": "Sunken Fossil World",
     "th18": "Unconnected Marketeers",
-    "th185": "100th Black Market"
+    "th185": "100th Black Market",
 }
 
 import vdf
@@ -33,26 +33,31 @@ import winreg
 import os
 import glob
 
+
 def fail(err):
     print(err)
     os.system("pause")
     exit()
+
+
 def makeSteamPath(path):
-    return '"' + path.replace('/', '\\') + '"'
+    return '"' + path.replace("/", "\\") + '"'
+
+
 def makeAppName(game, lang):
-    lang = lang.replace('.js', '')
+    lang = lang.replace(".js", "")
     if name := names.get(game):
         longGame = game
         if len(longGame) > 4:
-            longGame = longGame[:4] + '.' + longGame[4:]
-        longGame = longGame.replace('th', 'Touhou ', 1)
+            longGame = longGame[:4] + "." + longGame[4:]
+        longGame = longGame.replace("th", "Touhou ", 1)
         return f"{longGame} - {name} ({lang})"
     else:
         return f"{game} ({lang})"
 
+
 steamPath = winreg.QueryValueEx(
-    winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Software\\Valve\\Steam"),
-    "SteamPath"
+    winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Software\\Valve\\Steam"), "SteamPath"
 )[0]
 if not steamPath:
     fail("Failed to find Steam. Please run Steam at least once before continuing.")
@@ -60,32 +65,41 @@ if not os.path.exists("thcrap.exe"):
     if os.path.exists("../thcrap.exe"):
         os.chdir("..")
     else:
-        fail("Failed to find thcrap. Make sure this application is placed in the thcrap folder.")
+        fail(
+            "Failed to find thcrap. Make sure this application is placed in the thcrap folder."
+        )
 if not os.path.exists("config/games.js"):
-    fail("Failed to find thcrap config files. Please run thcrap.exe and configure your games before continuing.")
-games = json.load(open("config/games.js", "r", encoding='utf-8'))
-langs = [os.path.basename(x) for x in glob.glob("config/*.js") if os.path.basename(x) not in ["games.js", "config.js"]]
+    fail(
+        "Failed to find thcrap config files. Please run thcrap.exe and configure your games before continuing."
+    )
+games = json.load(open("config/games.js", "r", encoding="utf-8"))
+langs = [
+    os.path.basename(x)
+    for x in glob.glob("config/*.js")
+    if os.path.basename(x) not in ["games.js", "config.js"]
+]
 
 sFiles = glob.glob(os.path.join(steamPath, "userdata", "**", "config", "shortcuts.vdf"))
 if not sFiles:
-    fail("Failed to find any shortcuts.vdf files. Please create at least one shortcut in Steam (to any game, not necessarily Touhou) manually before continuing.")
+    fail(
+        "Failed to find any shortcuts.vdf files. Please create at least one shortcut in Steam (to any game, not necessarily Touhou) manually before continuing."
+    )
 
 for sFile in sFiles:
-    shorts = vdf.binary_load(open(sFile, "rb"))['shortcuts']
+    shorts = vdf.binary_load(open(sFile, "rb"))["shortcuts"]
     short_index = max([int(x) for x in shorts.keys()]) + 1
     for lang in langs:
         for game, icon in games.items():
             if "_custom" in game:
                 continue
-            name = names.get(game, '')
             short = {
-                'AppName': makeAppName(game, lang),
-                'Exe': makeSteamPath(os.path.join(os.getcwd(), 'thcrap_loader.exe')),
-                'icon': makeSteamPath(icon),
-                'LaunchOptions': f"{lang} {game}"
+                "AppName": makeAppName(game, lang),
+                "Exe": makeSteamPath(os.path.join(os.getcwd(), "thcrap_loader.exe")),
+                "icon": makeSteamPath(icon),
+                "LaunchOptions": f"{lang} {game}",
             }
             print(f"Adding {short['AppName']}...")
             shorts[str(short_index)] = short
             short_index += 1
-    vdf.binary_dump({'shortcuts': shorts}, open(sFile, "wb"))
+    vdf.binary_dump({"shortcuts": shorts}, open(sFile, "wb"))
     fail("All done. Please restart Steam to see the Touhou games in your library.")
