6fb1c57b58c25368c1ba5e4a87129b86333891e1
ooa113y <462484+ooa113y@users.noreply.github.com>
2023-01-02 00:54:48 +0400

    Code cleanup (no functional changes)

--- steam-thcrap.py
+++ steam-thcrap.py
@@ -77,72 +77,106 @@ def makeGridMap(short, game):
     return gd
 
 
-steamPath = winreg.QueryValueEx(
-    winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Software\\Valve\\Steam"), "SteamPath"
-)[0]
-if not steamPath:
-    fail("Failed to find Steam. Please run Steam at least once before continuing.")
-if not os.path.exists("thcrap.exe"):
-    if os.path.exists("../thcrap.exe"):
-        if os.path.exists("grid") and not os.path.exists("../grid"):
-            shutil.move("grid", "../grid")
-        os.chdir("..")
-    else:
+def getSteamPath():
+    steamPath = winreg.QueryValueEx(
+        winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Software\\Valve\\Steam"), "SteamPath"
+    )[0]
+    if not steamPath:
+        fail("Failed to find Steam. Please run Steam at least once before continuing.")
+    return steamPath
+
+
+def handleSubdirectory():
+    if not os.path.exists("thcrap.exe"):
+        if os.path.exists("../thcrap.exe"):
+            if os.path.exists("grid") and not os.path.exists("../grid"):
+                shutil.move("grid", "../grid")
+            os.chdir("..")
+        else:
+            fail(
+                "Failed to find thcrap. Make sure this application is placed in the thcrap folder."
+            )
+
+
+def getGames():
+    if not os.path.exists("config/games.js"):
         fail(
-            "Failed to find thcrap. Make sure this application is placed in the thcrap folder."
+            "Failed to find thcrap config files. Please run thcrap.exe and configure your games before continuing."
         )
-if not os.path.exists("config/games.js"):
-    fail(
-        "Failed to find thcrap config files. Please run thcrap.exe and configure your games before continuing."
+    games = json.load(open("config/games.js", "r", encoding="utf-8"))
+    langs = [
+        os.path.basename(x)
+        for x in glob.glob("config/*.js")
+        if os.path.basename(x) not in ["games.js", "config.js"]
+    ]
+    return games, langs
+
+
+def getShortcutFiles(steamPath):
+    sFiles = glob.glob(
+        os.path.join(steamPath, "userdata", "**", "config", "shortcuts.vdf")
     )
-games = json.load(open("config/games.js", "r", encoding="utf-8"))
-langs = [
-    os.path.basename(x)
-    for x in glob.glob("config/*.js")
-    if os.path.basename(x) not in ["games.js", "config.js"]
-]
+    if not sFiles:
+        fail(
+            "Failed to find any shortcuts.vdf files. Please create at least one shortcut in Steam (to any game, not necessarily Touhou) manually before continuing."
+        )
+    return sFiles
 
-sFiles = glob.glob(os.path.join(steamPath, "userdata", "**", "config", "shortcuts.vdf"))
-if not sFiles:
-    fail(
-        "Failed to find any shortcuts.vdf files. Please create at least one shortcut in Steam (to any game, not necessarily Touhou) manually before continuing."
-    )
 
-exePath = makeSteamPath(os.path.join(os.getcwd(), "thcrap_loader.exe"))
+def checkGameExists(shorts, lang, game, exePath):
+    for short in shorts.values():
+        if (
+            short.get("Exe") == exePath
+            and short.get("LaunchOptions") == f"{lang} {game}"
+        ):
+            return True
+    return False
 
-for sFile in sFiles:
+
+def copyGameArt(short, game, configDir):
+    for src, dst in makeGridMap(short, game).items():
+        shutil.copy(src, os.path.join(configDir, dst))
+
+
+def restartSteam(steamPath, timeout=10):
+    os.system(f'"{steamPath}\\steam.exe" -shutdown')
+    time.sleep(timeout)  # Wait for Steam to shut down
+    os.system("start steam://open/games")
+
+
+def addShortcut(sFile, game, lang, icon):
+    exePath = makeSteamPath(os.path.join(os.getcwd(), "thcrap_loader.exe"))
     configDir = os.path.dirname(sFile)
     userId = os.path.basename(os.path.dirname(configDir))
     shorts = vdf.binary_load(open(sFile, "rb"))["shortcuts"]
-    short_index = max([int(x) for x in shorts.keys()]) + 1
-    for lang in langs:
-        for game, icon in games.items():
-            if "_custom" in game:
-                continue
-            hasGame = False
-            for existingShort in shorts.values():
-                if (
-                    existingShort.get("Exe") == exePath
-                    and existingShort.get("LaunchOptions") == f"{lang} {game}"
-                ):
-                    hasGame = True
-                    break
-            if hasGame:
-                continue
-            short = {
-                "AppName": makeAppName(game, lang),
-                "Exe": exePath,
-                "icon": makeSteamPath(icon),
-                "LaunchOptions": f"{lang} {game}",
-            }
-            print(f"Adding {short['AppName']} for user {userId}...")
-            for src, dst in makeGridMap(short, game).items():
-                shutil.copy(src, os.path.join(configDir, dst))
-            shorts[str(short_index)] = short
-            short_index += 1
-    vdf.binary_dump({"shortcuts": shorts}, open(sFile, "wb"))
+    short = {
+        "AppName": makeAppName(game, lang),
+        "Exe": exePath,
+        "Icon": makeSteamPath(icon),
+        "LaunchOptions": f"{lang} {game}",
+    }
+    if not checkGameExists(shorts, lang, game, exePath):
+        print(f"Adding {short['AppName']} for user {userId}...")
+        copyGameArt(short, game, configDir)
+        short_index = str(max([int(x) for x in shorts.keys()]) + 1)
+        shorts[short_index] = short
+        vdf.binary_dump({"shortcuts": shorts}, open(sFile, "wb"))
 
-print("All done. Restarting Steam...")
-os.system(f'"{steamPath}\\steam.exe" -shutdown')
-time.sleep(10)  # Wait for Steam to shut down
-os.system("start steam://open/games")
+
+def main():
+    handleSubdirectory()
+    steamPath = getSteamPath()
+    games, langs = getGames()
+    sFiles = getShortcutFiles(steamPath)
+    for sFile in sFiles:
+        for lang in langs:
+            for game, icon in games.items():
+                if "_custom" not in game:
+                    addShortcut(sFile, game, lang, icon)
+
+    print("All done. Restarting Steam...")
+    restartSteam(steamPath)
+
+
+if __name__ == "__main__":
+    main()
